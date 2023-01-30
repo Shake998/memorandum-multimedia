b28f3ac0b719705ed506cbc3bdf61f6d61c82dd3
David Masip <davidmasip16@gmail.com>
2023-01-30 13:07:23 +0100

    Ejercicio 1
    Ejercicio 1:
    Ruta get '/'
    Middleware general

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 
 
+// Middleware
+app.use((req, res, next) => {
+  const mensaje = `Request a ${req.baseUrl} ${Date.now()}`
+  console.log(mensaje);
+  next()
+})
 
 
-
-
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    message: 'Este request/response está OK'
+   })
+})
 
 
 
